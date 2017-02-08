a19c82b9d7b6e2d2e65405ceba90e4720439c4b4
stefan.jovanovic <stefan.jovanovic@cs.ucalgary.ca>
2017-02-08 14:15:08 -0700

    working analog clock

--- Alarm Clock/MainWindow.xaml.cs
+++ Alarm Clock/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.Windows.Threading;
 
 namespace Alarm_Clock
 {
@@ -20,25 +22,57 @@ namespace Alarm_Clock
     /// </summary>
     public partial class MainWindow : Window
     {
+        //these are the objects of the min, hour, and second hand
+        private RotateTransform MinHandTr = new RotateTransform();
+        private RotateTransform HourHandTr = new RotateTransform();
+        private RotateTransform SecHandTr = new RotateTransform();
+
         public MainWindow()
         {
+            //initalizes the clock  
             InitializeComponent();
+
 
             System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            
             dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
             dispatcherTimer.Start();
 
+            //takes in the keyboard input and if it's "esc" it closes the program down
             this.KeyUp += MainWindow_KeyUp;
         }
 
+        /* this method is an event driven system for analog and digital clock
+         * event is called every "tick" which is one second
+         * it also updates the clocks each second
+         */
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
-            digitalTime.Content = DateTime.Now.ToString("hh:mm:ss");
-            amORpm.Content = DateTime.Now.ToString("tt");
-            date.Content = DateTime.Now.ToString("MMM dd, yyyy");
+
+            /* digital clock
+             * displays the time, date and am
+             */ 
+            digitalTime.Content = DateTime.Now.ToString("hh:mm:ss"); //hours:minutes: seconds   
+            amORpm.Content = DateTime.Now.ToString("tt"); //AM or PM 
+            date.Content = DateTime.Now.ToString("MMM dd, yyyy"); //month day, year 
+
+
+
+            //analog clock
+            //calculates angles for the minute, hour and second hand 
+            MinHandTr.Angle = (DateTime.Now.Minute * 6); 
+            HourHandTr.Angle = (DateTime.Now.Hour * 30) + (DateTime.Now.Minute * 0.5); 
+            SecHandTr.Angle = (DateTime.Now.Second * 6); 
+
+            //moves the minute, second and hour hand  
+            MinuteHand.RenderTransform = MinHandTr; 
+            HourHand.RenderTransform = HourHandTr;
+            SecondHand.RenderTransform = SecHandTr;
         }
 
+        /* This method closes the program down if the escape key is hit
+         */ 
         private void MainWindow_KeyUp(object sender, KeyEventArgs e)
         {
             if(e.Key == Key.Escape)
@@ -46,5 +80,6 @@ namespace Alarm_Clock
                 Application.Current.Shutdown();
             } 
         }
+
     }
 }
